da8370a37fa0e7fa4dc314d8baeb066a9f3b969b
Altamash <mdaltamashzzama@gmail.com>
2026-05-29 19:33:02 +0530

    Minor bug fix and model saving

--- LSTM RNN/main.py
+++ LSTM RNN/main.py
@@ -75,7 +75,19 @@ def predict_next_word(model, tokenizer, text, max_sequence_len):
         if index == predicted_word_index:
             return word
     return None
-    input_text="To be or not to be"
+input_text="To be or not to be"
+print(f"Input text:{input_text}")
+max_sequence_len=model.input_shape[1]+1
+next_word=predict_next_word(model,tokenizer,input_text,max_sequence_len)
+print(f"Next Word PRediction:{next_word}")
+
+## Save the model
+model.save("next_word_lstm.h5")
+## Save the tokenizer
+import pickle
+with open('tokenizer.pickle','wb') as handle:
+    pickle.dump(tokenizer,handle,protocol=pickle.HIGHEST_PROTOCOL)
+input_text="  Barn. Last night of all,When yond same"
 print(f"Input text:{input_text}")
 max_sequence_len=model.input_shape[1]+1
 next_word=predict_next_word(model,tokenizer,input_text,max_sequence_len)
