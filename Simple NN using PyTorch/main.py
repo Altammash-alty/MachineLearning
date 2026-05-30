ee633b629c764dc4cba8313fd8c55ed6d20b7158
Altamash <mdaltamashzzama@gmail.com>
2026-05-30 16:38:12 +0530

    Model using Pytorch

--- Simple NN using PyTorch/main.py
+++ Simple NN using PyTorch/main.py
@@ -1,6 +1,28 @@
 import torch 
 import torch.nn as nn
 import numpy as np
+from torch.utils.data import DataLoader,Dataset
+from sklearn.model_selection import train_test_split
+
+torch.manual_seed(37)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using Device : {device}')                   
+    
+
+#Crreating Dataset
+class  TrainingDataset(Dataset):
+    def __init__(self,features,labels): 
+        self.features=features
+        self.labels=labels
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self,idx):
+        return self.features[idx],self.labels[idx]
+
+
+
 
 class Model(nn.Module):
     def __init__(self,num_features):
