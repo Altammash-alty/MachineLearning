e47c6fb6c44460bebd18d002fd0eed6bdb03062f
Altamash <mdaltamashzzama@gmail.com>
2026-05-31 02:31:31 +0530

    Minor bug fixes

--- MNISTFashion/main.py
+++ MNISTFashion/main.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd      
+import optuna
 
 
 
@@ -43,21 +44,32 @@ TestDataset = MyDataset(X_test,y_test)
 
 #creating the nn model 
 class MLP(nn.Module):
-    def __init__(self,num_features):
+    def __init__(self, num_features, hidden_layers, number_of_neurons, activations, dropout_rate):
         super().__init__()
-        layers=[]
-        for i in range (hidden_layers):
-            layers.append(nn.Linear(number_features,number_of_neurons)),
-            layers.append(nn.BatchNorm1d(number_of_neurons)),
-            layers.append(nn.activations()),
-            layers.append(nn.Dropout(dropout_rate)),
-            number_features=number_of_neurons
+        layers = []
+        in_features = num_features
+        for i in range(hidden_layers):
+            layers.append(nn.Linear(in_features, number_of_neurons))
+            layers.append(nn.BatchNorm1d(number_of_neurons))
+            
+            # Select activation function
+            if activations == "ReLU":
+                layers.append(nn.ReLU())
+            elif activations == "Tanh":
+                layers.append(nn.Tanh())
+            elif activations == "Softmax":
+                layers.append(nn.Softmax(dim=1))
+            elif activations == "Leaky ReLU":
+                layers.append(nn.LeakyReLU())
+                
+            layers.append(nn.Dropout(dropout_rate))
+            in_features = number_of_neurons
 
-        layers.append(nn.Linear(number_of_neurons,10)),
-        layers.append(nn.Softmax())      
+        layers.append(nn.Linear(number_of_neurons, 10))
+        # Note: Softmax is removed because nn.CrossEntropyLoss expects raw logits.
         self.model = nn.Sequential(*layers)    
 
-    def forward(self,X):
+    def forward(self, X):
         return self.model(X)
 
 
@@ -71,7 +83,7 @@ def objective(trial):
     hidden_layer=trial.suggest_int("hidden_layer",1,10)
     number_of_neurons=trial.suggest_int("number_of_neurons",100,500,step=100)
     activation_functions=trial.suggest_categorical("activations",["ReLU","Tanh","Softmax","Leaky ReLU"])
-    optimiser=trial.suggest_categorical("optimiser",["RMSProp","Adam","SGD","Adagrad","AdamW"])
+    optimiser_name=trial.suggest_categorical("optimiser",["RMSProp","Adam","SGD","Adagrad","AdamW"])
     learning_rate=trial.suggest_float("learning_rate",1e-5,1e-1,log=True)
     dropout_rate=trial.suggest_float("dropout_rate",0.1,0.5,step=0.1)
     batch_size=trial.suggest_categorical("batch_size",[16,32,64,128])
@@ -82,51 +94,53 @@ def objective(trial):
     TrainDataloader=DataLoader(TrainDataset,batch_size=batch_size,shuffle=True,pin_memory=True)
     TestDataloader=DataLoader(TestDataset,batch_size=batch_size,shuffle=False,pin_memory=True)
     
+    #Model initialisation
+    model=MLP(
+        num_features=X_train.shape[1],
+        hidden_layers=hidden_layer,
+        number_of_neurons=number_of_neurons,
+        activations=activation_functions,
+        dropout_rate=dropout_rate
+    )
+    model.to(device)
+
     criterion=nn.CrossEntropyLoss()
 
-    if optimiser=="RMSProp":
+    if optimiser_name=="RMSProp":
         optimiser=optim.RMSprop(model.parameters(),lr=learning_rate,weight_decay=weight_decay)
-    elif optimiser=="Adam":
+    elif optimiser_name=="Adam":
         optimiser=optim.Adam(model.parameters(),lr=learning_rate,weight_decay=weight_decay)
-    elif optimiser=="SGD":
+    elif optimiser_name=="SGD":
         optimiser=optim.SGD(model.parameters(),lr=learning_rate,weight_decay=weight_decay)
-    elif optimiser=="Adagrad":
+    elif optimiser_name=="Adagrad":
         optimiser=optim.Adagrad(model.parameters(),lr=learning_rate,weight_decay=weight_decay)
     else :
         optimiser=optim.AdamW(model.parameters(),lr=learning_rate,weight_decay=weight_decay)
     
-    
+    model.train()
+    for epoch in range(epochs):
+        for batch_features , batch_labels in TrainDataloader:
+            batch_features=batch_features.to(device)
+            batch_labels=batch_labels.to(device)
 
-        #Model initialisation
-        model=MLP(X_train.shape[1])
-        model.to(device)
+            #Forward Pass 
+            outputs=model(batch_features)
 
+            #Calculate loss
+            #zero the gradients
+            optimiser.zero_grad()
+            loss=criterion(outputs,batch_labels)
 
-        for epoch in range(epochs):
-            for batch_features , batch_labels in TrainDataloader:
-                batch_features=batch_features.to(device)
-                batch_labels=batch_labels.to(device)
+            #BackPass
+            loss.backward()
 
-                #Forward Pass 
-                outputs=model(batch_features)
+            #upgrading the gradients
+            optimiser.step()
 
-                #Calculate loss
-                #zero the gradients
-                optimiser.zero_grad()
-                loss=criterion(outputs,batch_labels)
+    #Model Eval
+    model.eval()
 
-                #BackPass
-                loss.backward()
-
-                #upgrading the gradients
-                optimiser.step()
-                
-                
-
-        #Model Eval
-        model.eval()
-
-#Evaluation 
+    #Evaluation 
     total=0
     correct=0
     with torch.no_grad():
@@ -141,9 +155,12 @@ def objective(trial):
     accuracy = correct/total
 
     print(f'Accuracy obtained is : {accuracy}')
+    return accuracy
 
-study=optuna.create_study(direction="maximize",sampler="TPESampler")
-study.optimize("objective",n_trials=50)
+study=optuna.create_study(direction="maximize",sampler=optuna.samplers.TPESampler())
+study.optimize(objective,n_trials=50)
+print(f'Best Accuracy: {study.best_value}')
+print(f'Best Parameters : {study.best_params}')
 
 
         
