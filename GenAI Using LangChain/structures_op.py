f7290be92d1b1f056729d9747c2ebf2b8998f359
Altamash <mdaltamashzzama@gmail.com>
2026-06-10 21:08:55 +0530

    StrunctureOP

---
+++ GenAI Using LangChain/structures_op.py
@@ -0,0 +1,14 @@
+from langchain_huggingface import HuggingFaceEndpoint , HuggingFacePipeline , ChatHuggingFace
+from langchain.langchain_core.prompts import PromptTemplate , HumanMessage , AIMessage
+from langchain.output_parsers import StructureOutputParser , ResponseSchema
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+model=HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B",
+    task="conversational",
+    temperature=2.0,
+)
+
