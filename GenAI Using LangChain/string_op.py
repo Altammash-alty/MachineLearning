ff8cbc7895bbe4c9aef7d0d04e6a73b378c88f67
Altamash <mdaltamashzzama@gmail.com>
2026-06-10 20:36:39 +0530

    Output Parser

--- GenAI Using LangChain/string_op.py
+++ GenAI Using LangChain/string_op.py
@@ -1,3 +1,6 @@
 from langchain_huggingface import HuggingFaceEndpoint , HuggingFacePipeline
 from langchain.langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
+import os
+
+load_dotenv()
