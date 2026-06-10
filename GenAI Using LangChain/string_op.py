62d9bcf0986ceadfb85c271f1e7182268de314d9
Altamash <mdaltamashzzama@gmail.com>
2026-06-10 20:40:14 +0530

    Output Parser

--- GenAI Using LangChain/string_op.py
+++ GenAI Using LangChain/string_op.py
@@ -1,5 +1,5 @@
 from langchain_huggingface import HuggingFaceEndpoint , HuggingFacePipeline , ChatHuggingFace
-from langchain.langchain_core.prompts import PromptTemplate
+from langchain.langchain_core.prompts import PromptTemplate , HumanMessage , AIMessage
 from dotenv import load_dotenv
 import os
 
@@ -12,3 +12,13 @@ llm = HuggingFaceEndpoint(
 )
 
 model=ChatHuggingFace(llm=llm)
+
+
+#1st prompt 
+
+Prompt1=PromptTemplate(
+    template="Write a detailed description on the {topic}",
+    input_variables=["topic"],
+    validate_template=True
+)
+#2nd prompt
