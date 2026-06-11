3a39e7bca9df2d50e1a52bedfc4f90662cdfe633
Altamash <mdaltamashzzama@gmail.com>
2026-06-11 21:13:08 +0530

    Minor changes

--- GenAI Using LangChain/chains.py
+++ GenAI Using LangChain/chains.py
@@ -3,6 +3,12 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableParallel , RunnableBranch , RunnableLambda
 from langchain_core import Pydantic
 from Pydantic import BaseModel , Field
-
-
 from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="",
+    task="",
+    temperature=2
+)
