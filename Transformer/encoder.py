7ac0a067b609829c1c952c4506badc0dd0dacc46
Altamash <mdaltamashzzama@gmail.com>
2026-06-26 21:22:25 +0530

    Implementing Transformer from Scratch

--- Transformer/encoder.py
+++ Transformer/encoder.py
@@ -5,8 +5,9 @@ import torch.nn as nn
 
 
 class encode(nn.Module):
-    def __init__(self):
+    def __init__(self,vocab_size,n_dim,n_heads,n_layer,d_ff):
         super().__init__()
+        emb
 
     def forward(self,x):
         return logits
