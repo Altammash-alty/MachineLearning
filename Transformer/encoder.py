a223f43d0fde2c8a3c7b01067da28b1a9758b1af
Altamash <mdaltamashzzama@gmail.com>
2026-06-27 15:51:11 +0530

    Initialising the layers

--- Transformer/encoder.py
+++ Transformer/encoder.py
@@ -8,6 +8,7 @@ class encode(nn.Module):
     def __init__(self,vocab_size,n_dim,n_heads,n_layer,d_ff):
         super().__init__()
         self.embedding=nn.Embedding(vocab_size,n_dim)
+        self.dropout=nn.Dropout(0.1)
        
     def forward(self,x):
         return logits
