95166cbaade5806e77fda5657bc4404cbb2da876
Altamash <mdaltamashzzama@gmail.com>
2026-07-10 21:27:55 +0530

    ChatBot using LangGraph

--- LangGraph/main.py
+++ LangGraph/main.py
@@ -3,6 +3,6 @@ from typing import TypedDict
 from Pydantic import BaseModel , Field
 
 
-class evaluation_schema(BaseModel):
-    feedback : str = Field(description = "")
+class conversation_history(BaseModel):
+    messsages : Annotated[list[BaseMessage]]
     
