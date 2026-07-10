d84663daf4e65876e25e57868e9f2b57c1f0415a
Altamash <mdaltamashzzama@gmail.com>
2026-07-10 21:29:28 +0530

    ChatBot using LangGraph

--- LangGraph/main.py
+++ LangGraph/main.py
@@ -1,8 +1,9 @@
 from langgraph import StateGraph
 from typing import TypedDict
 from Pydantic import BaseModel , Field
-
+from langchain_core.messages import BaseMessage,HumanMessage,AIMessage
 
 class conversation_history(BaseModel):
-    messsages : Annotated[list[BaseMessage]]
+    """BaseMessage gives the flexibility to the user to store the conversation whether it is human message , AI or System """
+    messages : Annotated[list[BaseMessage]]
     
