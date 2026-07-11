ef39b9b142a920fc915a3345454c96e784bdae5b
Altamash <mdaltamashzzama@gmail.com>
2026-07-11 20:59:03 +0530

    ChatBot using LangGraph

--- LangGraph/main.py
+++ LangGraph/main.py
@@ -49,12 +49,14 @@ while True :
     if user_message.strip().lower() in ["quit","bye","exit"]:
         break
 
-    config={
+    
+    for message.chunk , metadata in  workflow.stream(
+        ({"message":HumanMessage(content=user_message)},config={
         "configurable":{
             "thread_id":thread_id
         }   
-    }
-    response = workflow.stream({"message":HumanMessage(content=user_message)},config=config, stream_mode='messages')
+    }, stream_mode='messages')):
 
-    for messsage.chunk , metadata in stream :
+    for messsage.chunk , metadata in workflow.stream :
+
     print(response['messages'][-1].content)
